c7153cac759a8d6a9a36cf1a9057a6ff67949b39
ErikTheCoder <50461511+ErikTheCoder@users.noreply.github.com>
2021-06-29 08:52:50 +0200

    Lots of stuffs

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function toTab(e) {
+    console.log(1)
+    document.getElementById(e).parentNode.querySelectorAll("#" + document.getElementById(e).parentNode.id + " > .view").forEach(function(element) {
+        element.classList.add('hidden');
+    });
+    document.getElementById(e).classList.remove('hidden');
+}
