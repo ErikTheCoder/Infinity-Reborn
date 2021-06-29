0dfe9a1b9b877b349fe1e75da282dbce323c6e34
ErikTheCoder <50461511+ErikTheCoder@users.noreply.github.com>
2021-06-29 15:13:24 +0200

    MADE GAMEPLAY

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+var peaks = 1;
+var neutralWaves = 0;
+var neutralWaveCost = 0;
+var neutralWaveCostMult = 2;
+var wavelength = 1;
+var speed = 1;
+var lastUpdate = Date.now();
 
 function toTab(e) {
     console.log(1)
@@ -8,3 +15,36 @@ function toTab(e) {
 }
 
 toTab('mainview');
+
+function buyNeutralWave(){
+    if(peaks >= neutralWaveCost){
+        peaks -= neutralWaveCost;
+        neutralWaves += 1;
+    }
+}
+
+function updateCosts(){
+    neutralWaveCost = neutralWaveCostMult**neutralWaves;
+}
+
+function updateDisplay(){
+    document.getElementById('numberDisplay').innerHTML = "You have reached " + peaks.toFixed(2); + " peaks";
+    document.getElementById('wavecosttextid').innerHTML = "Cost: " + neutralWaveCost; 
+}
+
+function productionLoop(diff){
+    speed = neutralWaves;
+    peaks += (speed / wavelength) * (diff / 1000);
+}
+
+
+function loop(){
+    diff = Date.now() - lastUpdate;
+    lastUpdate = Date.now();
+
+    updateCosts();
+    updateDisplay();
+    productionLoop(diff);
+}
+
+setInterval(loop, 20)
