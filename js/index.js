9e2688aae40d52b7b641511d0ce9faceacb8cde8
MrRedShark77 <mrredshark77@gmail.com>
2021-06-29 16:59:15 +0300

    fixed :/

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
-var peaks = 1;
-var neutralWaves = 0;
-var neutralWaveCost = 0;
-var neutralWaveCostMult = 2;
-var wavelength = 1;
-var speed = 1;
+const E = (x) => { return new ExpantaNum(x) }
+
 var lastUpdate = Date.now();
+
+const PLAYER_DATA = {
+    peaks: E(1),
+    neutralWaves: E(0),
+}
+
+var player = PLAYER_DATA
+var tab = ""
+var temp = {}
 
 function toTab(e) {
     console.log(1)
+    tab = e
     document.getElementById(e).parentNode.querySelectorAll("#" + document.getElementById(e).parentNode.id + " > .view").forEach(function(element) {
         element.classList.add('hidden');
     });
@@ -17,35 +23,59 @@ function toTab(e) {
 toTab('mainview');
 
 function buyNeutralWave(){
-    if(peaks >= neutralWaveCost){
-        peaks -= neutralWaveCost;
-        neutralWaves += 1;
+    if(player.peaks.gte(temp.neutralWaveCost)){
+        player.peaks = player.peaks.sub(temp.neutralWaveCost)
+        player.neutralWaves = player.neutralWaves.add(1)
     }
 }
 
 function updateCosts(){
-    neutralWaveCost = neutralWaveCostMult**neutralWaves;
+    temp.neutralWaveCost = E(2).pow(player.neutralWaves)
 }
 
 function updateDisplay(){
-    document.getElementById('numberDisplay').innerHTML = "You have reached " + peaks.toFixed(2) + " peaks";
-    document.getElementById('wavecosttextid').innerHTML = "Cost: " + neutralWaveCost;
-    document.getElementById('firstwaveamount').innerHTML = "You have " + neutralWaves + " neutral waves"
+    if (tab == "mainview") {
+        document.getElementById('peaksAmount').innerHTML = "You have reached " + format(player.peaks, 1) + " peaks";
+        document.getElementById('peaksGain').innerHTML = "Your wave is moving at " + format(temp.peaksGain, 1) + " m/s";
+        document.getElementById('wavelength').innerHTML = "Your wavelength is " + format(temp.wavelength, 1) + " m";
+        document.getElementById('wavecosttextid').innerHTML = "Cost: " + format(temp.neutralWaveCost, 0);
+        document.getElementById('firstwaveamount').innerHTML = "You have " + format(player.neutralWaves, 0) + " neutral waves";
+    }
 }
 
 function productionLoop(diff){
-    speed = neutralWaves;
-    peaks += (speed / wavelength) * (diff / 1000);
+    temp.wavelength = E(1);
+    temp.speed = player.neutralWaves
+    temp.peaksGain = temp.speed.div(temp.wavelength)
+    player.peaks = player.peaks.add(temp.peaksGain.mul(diff/1000))
 }
 
+function format(ex, acc=3) {
+    ex = E(ex)
+    if (ex.isInfinite()) return 'Infinity'
+    let e = ex.log10().floor()
+    if (e.lt(9)) {
+        if (e.lt(3)) {
+            return ex.toFixed(acc)
+        }
+        return ex.floor().toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    } else {
+        if (ex.gte("eeee9")) {
+            let slog = ex.slog()
+            return (slog.gte(1e9)?'':E(10).pow(slog.sub(slog.floor())).toFixed(3)) + "F" + format(slog.floor(), 0)
+        }
+        let m = ex.div(E(10).pow(e))
+        return (e.log10().gte(9)?'':m.toFixed(3))+'e'+format(e,0)
+    }
+}
 
 function loop(){
     diff = Date.now() - lastUpdate;
     lastUpdate = Date.now();
 
     updateCosts();
-    updateDisplay();
     productionLoop(diff);
+    updateDisplay();
 }
 
 setInterval(loop, 20)
